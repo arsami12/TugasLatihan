c90506f59a36fadfdf72a4199c756099640f1a02
kendi ardi <arsamisapper12@gmail.com>
2017-11-06 07:44:46 +0800

    Input Hitungan Luas Persegi Panjang

--- Latihan2.cpp
+++ Latihan2.cpp
@@ -9,6 +9,12 @@ int Al, Pa, Le, Ti, L ;
 	cout << "P : Persegi Panjang"<<endl ;
 	cout << "S : Segitiga"<<endl ;
 	cout << "Luas Hitung P atau S ? "; cin >> Option;
+	
+	if (Option=='P'){
+		system ("cls");
+	cout << "Hitung Luas Persegi Panjang"<<endl ;
+	cout <<""<<endl;
 
+}
 
 }
