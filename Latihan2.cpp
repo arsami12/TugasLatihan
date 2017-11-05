644b5ed4a38f8af0499484a3107175e97e200311
kendi ardi <arsamisapper12@gmail.com>
2017-11-06 07:45:59 +0800

    Hitungan Panjang Persegi

--- Latihan2.cpp
+++ Latihan2.cpp
@@ -14,7 +14,7 @@ int Al, Pa, Le, Ti, L ;
 		system ("cls");
 	cout << "Hitung Luas Persegi Panjang"<<endl ;
 	cout <<""<<endl;
-
+		cout << "Berapa Panjang Persegi Panjangnya ? "; cin >> Pa ;
 }
 
 }
