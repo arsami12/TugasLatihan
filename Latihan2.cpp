55b4f96ef40b2d4653e06a78b5a7bbcb48e9a37d
kendi ardi <arsamisapper12@gmail.com>
2017-11-06 07:41:17 +0800

    Input Masukan

--- Latihan2.cpp
+++ Latihan2.cpp
@@ -6,4 +6,9 @@ int main () {
 char Option, P, S ;
 int Al, Pa, Le, Ti, L ;
 
+	cout << "P : Persegi Panjang"<<endl ;
+	cout << "S : Segitiga"<<endl ;
+	cout << "Luas Hitung P atau S ? "; cin >> Option;
+
+
 }
