f27d3885f16e40bb699a590f8f53f18d34583dc7
kendi ardi <arsamisapper12@gmail.com>
2017-11-06 09:17:48 +0800

    Inputan If

--- Latihan5.cpp
+++ Latihan5.cpp
@@ -8,5 +8,10 @@ int T, B ;
 	cout << " Tinggi Badan Kamu (Cm) ? : "; cin >> T ;
 	cout << " Berat  Badan Kamu (Kg) ? : "; cin >> B ;
 	
+if (B<T/2.5){
+		system ("cls");
+		cout <<""<<endl;	
+	
+}
 
 }
