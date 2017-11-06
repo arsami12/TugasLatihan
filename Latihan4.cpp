3e0f990a11a03fbd34533c36fd6eb97452e295bf
kendi ardi <arsamisapper12@gmail.com>
2017-11-06 08:55:38 +0800

    Inputan Keterangan Positif

--- Latihan4.cpp
+++ Latihan4.cpp
@@ -6,4 +6,12 @@ int main () {
 int B ;
 
 	cout << "Angka Bilangan	: "; cin >> B ;
+	
+if (B>0){
+		system("cls");
+	cout << ""<<endl;
+	cout << "Well... Angka Bilangannya Adalah '!POSITIF!' ( "<<B<<" )!!"<<endl;	
 }
+
+}
+
